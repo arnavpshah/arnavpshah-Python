93d3f81857d70ea4384c415888fd459cbe02ce06
arnavpshah <62448161+arnavpshah@users.noreply.github.com>
2020-05-31 18:43:59 -0400

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,29 @@
+x = 15
+y = 4
+def addition():
+    print("x + y =", x+y)
+def subtraction():
+    print("x - y =", x-y)
+def multiplication():
+    print("x * y =", x*y)
+def division():    
+    print("x / y =", x/y)
+def floor_division():
+    print("x // y =", x//y)
+def exponentiate():
+    print("x ** y =", x**y)
+
+# Square root program
+baseNumber = 625
+
+print(baseNumber ** 0.5)
+
+a = 5
+b = 6
+c = 7
+
+s = (a+b+c)/2
+
+area = ((s-a)*(s-b)*(s-c)) ** 0.5
+
+print(f"The semi-perimeter is {s} and the are of the triangle is {area}.") 
