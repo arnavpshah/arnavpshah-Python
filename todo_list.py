57123b49763edc7002776b96a43ce1ec33937825
arnavpshah <62448161+arnavpshah@users.noreply.github.com>
2020-05-31 21:24:34 -0400

    Update todo_list.py

--- todo_list.py
+++ todo_list.py
@@ -1,23 +1,30 @@
-
 todo_tasks = []
+completed_tasks = []
 
 def Todo_list():
     exit_loop = True
     while (exit_loop):
         choice = str(input("Type a, r, p, q, or c. a to add, r to remove, p to print, q to quit or c to complete\n"))
         if (choice == "a"):
-            task = str(input("Type task to add\n"))
-            todo_tasks.append(task)
+            add_task = str(input("Type a task to add\n"))
+            todo_tasks.append(add_task)
         elif (choice == "r"):
-            print("Remove task")
+            remove_task = str(input("Type the task and we will remove it\n"))
+            for task in todo_tasks:
+                if (task == remove_task):
+                    todo_tasks.remove(remove_task)
         elif (choice == "p"):
-            print(todo_tasks)
+            print("Todo list:", todo_tasks)
+            print("Completed list:", completed_tasks)
         elif (choice == "q"):
             print("Exiting from app")
             exit_loop = False
         elif (choice == "c"):
-            completed_tasks = str(input("Which one is a completed task\n"))
-            print(completed_tasks , "- Completed")
+            finished_task = str(input("Which one is a completed task\n"))
+            for task in todo_tasks:
+                if (task == finished_task):
+                    todo_tasks.remove(finished_task)
+                    completed_tasks.append(finished_task)
         else:
                 print("That is not a valid input")
     
